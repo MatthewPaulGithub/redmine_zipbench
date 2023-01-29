0c6aee14d04c7c5b02d1a450858d3f98b1249037
MatthewPaulGithub <mpaul@cityassets.com>
2023-01-29 11:43:13 -0500

    Initial creation
    Zipbench plugin tries to add functionality to Redmine 5 to make it a collab platform

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# redmine_zipbench_github
+# redmine_zipbench
+
+## Summary
+
+* A bunch of stuff to make Redmine 5 into a Zipbench collab platform
+
+## Installation
+
+* git clone git://github.com/MatthewPaulGithub/redmine_zipbench.git
+* Then go run the install_zipbench_plugins in the /lib/scripts directory
+* Remember to do the various manual setups afterwards
+
  
--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_zipbench do
   name 'Redmine Zipbench plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Matthew Paul'
+  description 'Redmine 5 plugin to add Zipbench various functionality'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/MatthewPaulGithub/redmine_zipbench'
+  author_url 'https://github.com/MatthewPaulGithub'
 end
